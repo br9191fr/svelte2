2a67315a4857727d41a7af8090de65d69058a203
Bruno Ricci <brricci@orange.fr>
2023-12-16 18:44:21 +0100

    Base project

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,26 @@
 </script>
 
 <style>
+    header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 4rem;
+        background: #cf0056;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 2px 6px rgba(0,0,0,0.26);
+    }
+    h1 {
+        color: white;
+        font-family: 'Roboto Slab', serif;
+        margin: 0;
+    }
 
 </style>
-
+<header>
+    <h1>Meet us</h1>
+</header>
 <h1>Time for a project!</h1>
