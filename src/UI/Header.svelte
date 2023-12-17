b4d58d310ad7a64c3237542e5960c7f2660c4cfe
Bruno Ricci <brricci@orange.fr>
2023-12-17 09:55:19 +0100

    After lesson 47

---
+++ src/UI/Header.svelte
@@ -0,0 +1,23 @@
+<style>
+    header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 4rem;
+        background: #cf0056;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 2px 6px rgba(0,0,0,0.26);
+    }
+    h1 {
+        color: white;
+        font-family: 'Roboto Slab', serif;
+        margin: 0;
+    }
+
+</style>
+<header>
+    <h1>Meet us</h1>
+</header>
